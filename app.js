7320ec867909787b080bc6e3426e4521b8871b94
pradeep-jais <pradeepjais13@gmail.com>
2025-04-08 10:53:21 +0530

    feture: add new todo animated UI, POST request to json-server, get updated data and display

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-// Promise using then and catch
-// fetch(' http://localhost:3000/todos')
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     todos = data;
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
+const API_URL = 'http://localhost:3000';
 
-// Getting data(todos) from local json server
+const addTaskBtn = document.querySelector('.add-task');
 
 const todosContainer = document.querySelector('.todos');
 
 const getTodos = async () => {
+  todosContainer.innerHTML = "<p style='padding:2rem;'>Loading...</p>";
   try {
     const res = await fetch('http://localhost:3000/todos');
     const data = await res.json();
@@ -74,5 +64,47 @@ todosContainer.addEventListener('click', (e) => {
   if (btn.classList.contains('remove-btn')) {
     const id = btn.parentElement.dataset.id;
     deleteItem(id);
+    getTodos();
+  }
+});
+
+// Add new task
+
+const addTask = async (todo) => {
+  try {
+    const res = await fetch(`${API_URL}/todos`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    });
+    const data = await res.json();
+    // console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+addTaskBtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const inputField = e.currentTarget.previousElementSibling;
+
+  if (!inputField.classList.contains('active')) {
+    inputField.classList.add('active');
+    setTimeout(() => {
+      inputField.focus();
+    }, 500);
+  }
+
+  if (inputField.value) {
+    const newTask = {
+      id: new Date().getTime(),
+      todo: inputField.value,
+    };
+
+    await addTask(newTask);
+    await getTodos();
+    inputField.value = '';
   }
 });
